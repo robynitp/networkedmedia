18b878a780609729c04d57582bf4ca29d1cf1b2b
Robyn <rao218@nyu.edu>
2014-10-28 00:07:43 -0400

    Added in-class HTML and two servi examples

---
+++ 2-servi/servi-file-server/server.js
@@ -0,0 +1,11 @@
+// serve files from a directory named "public"
+serveFiles('public');
+
+/*
+ 
+NOTE: Make sure to create a directory in your Servi project called "public"
+and put at least one HTML file in it. 
+
+If you include a file called "index.html", it will show up automatically as your home page.
+
+ */
