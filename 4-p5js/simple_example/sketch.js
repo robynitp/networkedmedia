4f20ef830e731a8a97fff1f617895c9b9d050d0e
Robyn <rao218@nyu.edu>
2014-11-02 13:02:25 -0500

    p5 basic examples

---
+++ 4-p5js/simple_example/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+
+	console.log('setup.');
+	createCanvas(600, 400);
+
+}
+
+function draw() {
+
+	console.log('draw!');
+
+	// background
+	background(42, 169, 217);
+
+	// ellipse
+	fill(242, 228, 21);
+	ellipse(100,100,100,100);
+
+	//rectangle
+	fill(162, 217, 39);
+	rect(300,100,150,150);
+
+}
