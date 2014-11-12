23e0dfaffea207dc43afe73a1f74d20e3b63b4e7
Robyn <rao218@nyu.edu>
2014-11-11 22:58:01 -0500

    add ajax api examples

---
+++ 8-ajax/02capitol_words/sketch.js
@@ -0,0 +1,42 @@
+var baseUrl = "http://capitolwords.org/api/1/phrases.json?entity_type=state&entity_value=NY&apikey=";
+var apiKey = "3e16c02bbb374110b558027c6adb189d";
+var fullUrl = baseUrl+apiKey;
+var words;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+  loadJSON(fullUrl, getWords); 
+  // set up text size
+  textSize(32);
+
+}
+/*
+// You could use the draw function to make the words re-render continuously
+function draw(){
+  renderWords();  
+}*/
+
+function mousePressed(){
+  renderWords();
+}
+
+function getWords(json){
+  console.log(json); // for debugging, to view the full json result 
+  words = json;  
+  renderWords();
+}
+
+function renderWords(){
+  background(255);
+  // don't start drawing until there are words in the array
+  if (words.length > 0){  
+    console.log('got words');
+    // words is an array of objects
+    for (var i=0; i<words.length; i++){
+      fill(random(0,85), random(85,170), random(0,255), random(80,100));
+      // the actual word is stored in the ngram property of the word object
+      text(words[i].ngram,random(width),random(height));
+    }    
+  } 
+}
