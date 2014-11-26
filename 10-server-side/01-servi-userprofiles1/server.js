ce8d4e93babf1a3752102c450ca11caf5f310095
Robyn <robyn@nyu.edu>
2014-11-26 10:41:25 -0600

    minor change from in-class

--- 10-server-side/01-servi-userprofiles1/server.js
+++ 10-server-side/01-servi-userprofiles1/server.js
@@ -3,15 +3,21 @@ var servi = require('servi');
 var app = new servi(true);
 
 // set the port (defaults to 3000 if you leave out this line)
-//port(3111);
+port(3111);  // make sure "port()" line comes before "start()"
 start();
 // set up the routes
 route('/',showHome);
-route('/profile/:username',showProfile);
+route('/about',myAboutPage);
 
 function showHome(request){
 	request.respond("This is the home page.");
 }
+
+function myAboutPage(request){
+	request.respond("<h1>About Me!!!</h1>");
+}
+
+route('/profile/:username',showProfile);
 
 function showProfile(request){
 	var content = "<h1>" + request.params.username + "</h1>";
