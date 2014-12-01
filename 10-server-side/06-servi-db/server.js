8b5d8d187dd58cba2d01b550c2f6c27f72671cb1
Robyn <robyn@nyu.edu>
2014-11-30 22:05:38 -0500

    new servi db example

--- 10-server-side/06-servi-db/server.js
+++ 10-server-side/06-servi-db/server.js
@@ -10,18 +10,18 @@ route('/', showAll);
 route('/add/:person',addName);
 
 /* 
-    ==== Add a new name to the database ====
+==== Add a new name to the database ====
 
-	If the URL is http://123.com/add/Jane, then the name "Jane" gets added to the DB
-	The text "Jane" is added in an object with a property called "name", like this:
+If the URL is http://123.com/add/Jane, then the name "Jane" gets added to the DB
+The text "Jane" is added in an object with a property called "name", like this:
 	{
 	   name: "Jane"
 	}
-	Notice the flow of events and data:
-	1. Define a route, '/add/:person', where ':person' is a variable 
-	2. User visits that URL, as in http://123.com/add/Jane
-	3. Grab the variable used in place of ':person', using 'request.params.person'. 
-	   In this example, 'request.params.person' is equal to 'Jane'
+Notice the flow of events and data:
+    1. Define a route, '/add/:person', where ':person' is a variable 
+    2. User visits that URL, as in http://123.com/add/Jane
+    3. Grab the variable used in place of ':person', using 'request.params.person'. 
+       In this example, 'request.params.person' is equal to 'Jane'
 */
 function addName(request){
 	namesDB.add({name:request.params.person});
@@ -37,8 +37,7 @@ function showAll(request){
 		    namestext += names[i].name + "<br/>";
 		}
 		request.respond( namestext );
-	}
-	
+	}	
 }
 
 start();
