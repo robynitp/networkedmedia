35f2b104e512fb615fc800a77821cb8f065a07df
Robyn <robyn@nyu.edu>
2015-03-10 14:42:27 -0400

    added servi api example

---
+++ week6/13-servi_get_api/servi_http_get.js
@@ -0,0 +1,17 @@
+var servi = require('servi');
+var app = new servi(true);
+var http = require('http');
+var url = "http://api.openweathermap.org/data/2.5/weather?q=NewYork,USA";
+http.get(url, function(res) {
+    var data = "";
+    res.on('data', function (chunk) {
+      data += chunk;
+    });
+    res.on("end", function() {
+      console.log(data); // <-- this is the JSON
+      // do stuff with the JSON data here
+    });
+  }).on("error", function(e) {
+    console.log("Got error: " + e.message);
+  });
+
